df6e81a43e7a252cd84bf92ced1a9c281262a927
Garrett Sutula <garrettsutula@gmail.com>
2021-12-28 21:43:16 -0500

    Add todos

--- src/cli/init.ts
+++ src/cli/init.ts
@@ -1,3 +1,9 @@
 export async function initializeProject() {
+    // TODO
+    // Accept project name as only arg
+    // Create folder in current directory or throw error
+    // Create diagrams subfolder structure
+    // Create app.ts in main folder
+    // Create index.ts in subfolders
     throw new Error("init command isn't implemented yet.");
 }
