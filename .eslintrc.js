015be334bc7438bc7580679362becd2366d6fd37
Garrett Sutula <garrettsutula@gmail.com>
2022-01-05 18:29:01 -0500

    Update eslint config to resolve parser error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,21 @@
 module.exports = {
     env: {
-      es2021: true,
       node: true,
     },
     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
-      ecmaVersion: 13,
-      sourceType: 'module',
+      ecmaVersion: 6,
+      project: './tsconfig.json'
     },
     plugins: ['@typescript-eslint'],
     rules: {},
-    extends: [ 'airbnb-base', 'airbnb-typescript/base'],
-    parserOptions: {
-      project: './tsconfig.json'
-    },
+    extends: ['airbnb-base', 'airbnb-typescript/base'],
     rules: {
       'no-underscore-dangle': [0],
       'no-console': [0],
       'max-len': [2, 140],
       'import/prefer-default-export': [0]
-    }
+    },
+    ignorePatterns: ["dist/**/*"]
   }
