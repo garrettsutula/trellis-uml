706f052b83d9b924d35d790e92a5ba676d033af2
Garrett Sutula <garrettsutula@gmail.com>
2022-01-04 20:43:04 -0500

    Improve error handling, console logging

--- src/cli/build.ts
+++ src/cli/build.ts
@@ -1,40 +1,47 @@
 import * as path from 'path';
 import { access, mkdir } from 'fs/promises';
-import { execSync } from 'child_process';
+import { exec, execSync } from 'child_process';
 import { generateSystemDiagrams, escapeString } from '../common';
 import { DiagramRoot } from '../models';
 
-const scriptPath = __dirname;
 const workingDirectoryPath = process.cwd();
 const currentFolder = path.basename(workingDirectoryPath);
 
 export async function buildProject() {
     // Make sure current diagram code is compiled.
+    console.time(`Running 'npm run build' on current directory ${currentFolder} in`);
     try {
         execSync('npm run build');
     } catch (e) {
-        throw new Error(`Failed to build using project's build script. ${JSON.stringify(e)}`);
+        throw new Error(`Failed to build using project's build script.`);
     }
+    console.timeEnd(`Running 'npm run build' on current directory ${currentFolder} in`);
     
     // Test for output directory structure & create if needed.
+    console.time(`Made sure output directories are created by testing access to: ${currentFolder}/diagrams in`);
     try {
         await access('./diagrams');
     } catch (e) {
+        console.warn(`Directory '${currentFolder}/diagrams' not present, attempting to create it & child directories.`);
         await mkdir(path.join(workingDirectoryPath, './diagrams'));
         await mkdir(path.join(workingDirectoryPath, './diagrams/systems'));
         await mkdir(path.join(workingDirectoryPath, './diagrams/services'));
         await mkdir(path.join(workingDirectoryPath, './diagrams/solutions'));
         await mkdir(path.join(workingDirectoryPath, './diagrams/domains'));
     }
+    console.timeEnd(`Made sure output directories are created by testing access to: ${currentFolder}/diagrams in`);
     // Load compiled diagram source code as diagram root
+    console.time(`Loaded diagram root: ${currentFolder}/dist/app.js in`);
     const diagramPath = path.join(workingDirectoryPath, './dist/app.js');
     const diagramRoot: DiagramRoot = await import(path.join(workingDirectoryPath, './dist/app.js'));
+    console.timeEnd(`Loaded diagram root: ${currentFolder}/dist/app.js in`);
     // System Diagram Generation
-    Object.values(diagramRoot.systems).forEach((system, i, arr) => {
-        try {
-            generateSystemDiagrams(system, path.join(workingDirectoryPath, `./diagrams/systems/${escapeString(system.name)}.puml`));
-        } catch(e) {
-            throw new Error(`Diagram generation for system ${system.name} failed. ${JSON.stringify(e)}`);
-        }
-    });
+    console.log(`Generating system digrams for: ${Object.values(diagramRoot.systems).reduce((systemList, system) => systemList + `${system.name}, `, '')}`);
+    console.time('Generation completed in')
+    Object.values(diagramRoot.systems)
+        .map((system) => {
+            const diagramOutputPath = path.join(workingDirectoryPath, `./diagrams/systems/${escapeString(system.name)}.puml`);
+            return generateSystemDiagrams(system, diagramOutputPath);
+        });
+     console.timeEnd('Generation completed in')
 }
--- src/common/generate.ts
+++ src/common/generate.ts
@@ -4,18 +4,39 @@ import { generateComponentDiagram } from "../plantuml/component-diagram";
 import { generateNetworkDiagram } from "../plantuml/network-diagram";
 import { generateSystemDiagram } from "../plantuml/system-diagram";
 import { generateDeploymentDiagram } from "../plantuml/deployment-diagram";
-import { escapeString } from "./utils";
-import * as Path from 'path';
 
 
 export function generateSystemDiagrams(system: System, path: string) {
-    const diagrams = [
-        generateNetworkDiagram(system),
-        generateDeploymentDiagram(system),
-        generateComponentDiagram(system),
-        generateSystemDiagram(system),
-    ];
+    const diagrams = [];
+    try {
+        diagrams.push(generateNetworkDiagram(system))
+    } catch(e) {
+        throw new Error(`Error generating network diagrams.`);
+    }
+
+    try {
+        diagrams.push(generateDeploymentDiagram(system))
+    } catch(e) {
+        throw new Error(`Error generating deployment diagrams.`);
+    }
+
+    try {
+        diagrams.push(generateComponentDiagram(system))
+    } catch(e) {
+        throw new Error(`Error generating component diagrams.`);
+    }
+
+    try {
+        diagrams.push(generateSystemDiagram(system))
+    } catch(e) {
+        throw new Error(`Error generating system diagrams.`);
+    }
 
     const output = diagrams.join("\n''''''''''''''''''''''''''''''\n");
-    writeFileSync(path, output);
+
+    try {
+        writeFileSync(path, output);
+    } catch (e) {
+        throw new Error(`Error writing diagrams to filesystem.`);
+    }
 }
