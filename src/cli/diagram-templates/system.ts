2794e84524e731c7a8a867408a0274350df97857
Garrett Sutula <garrettsutula@gmail.com>
2022-01-06 21:41:34 -0500

    Implement `generate` for system diagrams

---
+++ src/cli/diagram-templates/system.ts
@@ -0,0 +1,20 @@
+export default `import { system } from "trellisuml";
+// Import components & relationships from other diagrams as needed, don't duplicate components in other diagram files!
+
+// Export individual components one at a time or with array destructuring.
+// Like:   'export const myService = service('My Service');'
+// Or:     'export const [myService, myOtherService] = service([{label: 'My Service'}, {label: 'My Other Service'}]);'
+
+export const componentRelationships = [
+
+];
+
+export default system({
+    name,
+    components: [
+      // Add all components defined in this system here.
+    ],
+    componentRelationships,
+});
+
+`;
