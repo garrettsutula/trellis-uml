3d2514ec476ffa8e17fafca60f5b5a8fc64182b3
Garrett Sutula <garrettsutula@gmail.com>
2022-01-12 23:10:25 -0500

    Remove example project

--- example-project/src/systems/eventbus_system.ts
+++
@@ -1,9 +0,0 @@
-import { system, queue } from "trellisuml";
-import { k8sCluster } from "../domains/domain";
-
-export default system({
-    name: "Event Bus",
-    components: {
-        eventBus: queue("Event Bus (Pub/Sub)", k8sCluster),
-    },
-})
--- example-project/src/systems/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as basketSystem } from './basket_system';
-export { default as catalogSystem } from './catalog_system';
-export { default as eventbusSystem } from './eventbus_system';
-export { default as exampleSystem } from './example_system';
-export { default as idSystem } from './identity_system';
-export { default as eshopSystem } from './microsoft_eshop';
-export { default as orderSystem } from './ordering_system';
