1e311bebdfded7d010513e86adf2eb8004e91839
Garrett Sutula <garrettsutula@gmail.com>
2022-01-19 19:50:45 -0500

    Add system diagram generation

--- src/generators/diagram/build.ts
+++ src/generators/diagram/build.ts
@@ -3,6 +3,7 @@ import { Solution, System } from '../../models';
 import { buildComponentDiagram } from './component-diagram';
 import { buildNetworkDiagram } from './network-diagram';
 import { buildDeploymentDiagram } from './deployment-diagram';
+import { buildSystemDiagram } from './system-diagram';
 
 export function build(input: Solution, path: string);
 export function build(input: System, path: string);
@@ -11,19 +12,27 @@ export function build(input: any, path:string) {
   try {
     diagrams.push(buildNetworkDiagram(input));
   } catch (e) {
-    throw new Error('Error generating network diagrams.');
+    throw new Error('Error generating network diagram.');
   }
 
   try {
     diagrams.push(buildDeploymentDiagram(input));
   } catch (e) {
-    throw new Error('Error generating deployment diagrams.');
+    throw new Error('Error generating deployment diagram.');
   }
 
   try {
     diagrams.push(buildComponentDiagram(input));
   } catch (e) {
-    throw new Error('Error generating component diagrams.');
+    throw new Error('Error generating component diagram.');
+  }
+
+  if (input instanceof Solution) {
+    try {
+      diagrams.push(buildSystemDiagram(input));
+    } catch (e) {
+      throw new Error('Error generating system diagram.');
+    }
   }
 
   const output = diagrams.join("\n''''''''''''''''''''''''''''''\n");
