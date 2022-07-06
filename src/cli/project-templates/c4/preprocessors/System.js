ae37e0c4f56ef178b1aa3fff053da2fe29de5175
Garrett Sutula <garrettsutula@gmail.com>
2022-07-06 12:01:50 -0400

    Bugfixes based on modeling test project

--- src/cli/project-templates/c4/preprocessors/System.js
+++ src/cli/project-templates/c4/preprocessors/System.js
@@ -1,5 +1,6 @@
 module.exports  = async (schema) => {
-  Object.keys(schema.components).forEach((componentKey) => {
+  const { components = [] } = schema;
+  Object.keys(components).forEach((componentKey) => {
     const component = schema.components[componentKey];
     // Adds a back-reference to each component so system name is easily accessible from {component}.parent.name
     component.parent = {$ref: "#"};
