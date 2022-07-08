378a4472595a0f3e297686c882c5106b83bf2a78
Garrett Sutula <garrettsutula@gmail.com>
2022-07-08 13:42:26 -0400

    Update preprocessors to use nameToId utility function, type.

--- src/cli/project-templates/c4/preprocessors/Component.js
+++ src/cli/project-templates/c4/preprocessors/Component.js
@@ -1,22 +1,21 @@
-function nameToId(name) {
-  return name.replace(/_{2}|_|\*|\(|\)/g, "").replace(/\n| |-|_|\.|:{2}|_{3}/g, "");
-}
+const nameToId = require('./lib/nameToId');
 
 module.exports = async (schema) => {
-  schema.id = nameToId(schema.name);
   const { elements = [] } = schema;
   Object.keys(elements).forEach((elementKey) => {
     const element = schema.elements[elementKey];
     // Adds a back-reference to each component so system name is easily accessible from {component}.parent.name
     element.parent = { $ref: '#' };
-    element.id = nameToId(elementKey); // TODO: sanitizer util
+    element.id = nameToId(elementKey);
+    element.type = 'codeElement';
     const { relationships = [] } = element;
     relationships.forEach((relationship) => {
       // Indicate if the reference is external, useful for conditional output in templates.
       relationship.isExternalRef = !relationship.target.$ref.startsWith('#');
     });
   });
-  // Adds system id
-  schema.id = nameToId(schema.name); // TODO: sanitizer util
+  schema.parent = { $ref: '#' };
+  schema.id = nameToId(schema.name);
+  schema.type = 'component';
   return schema;
 };
--- src/cli/project-templates/c4/preprocessors/System.js
+++ src/cli/project-templates/c4/preprocessors/System.js
@@ -1,12 +1,15 @@
-module.exports  = async (schema) => {
+const nameToId = require('./lib/nameToId');
+
+module.exports = async (schema) => {
   const { components = [] } = schema;
   Object.keys(components).forEach((componentKey) => {
     const component = schema.components[componentKey];
     // Adds a back-reference to each component so system name is easily accessible from {component}.parent.name
-    component.parent = {$ref: "#"};
-    component.id = component.name; // TODO: sanitizer util
+    component.parent = { $ref: '#' };
+    component.id = nameToId(component.name);
+    component.type = 'component';
   });
-  // Adds system id
-  schema.id = schema.name; // TODO: sanitizer util
+  schema.id = nameToId(schema.name);
+  schema.type = 'system';
   return schema;
-}
+};
