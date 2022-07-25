8b14726c374d30ef6bff167dd8576dce897d545f
Garrett Sutula <garrett.sutula@gmail.com>
2022-07-24 21:12:31 -0400

    Add helpers as separate modules

---
+++ src/cli/helpers/ifCond.ts
@@ -0,0 +1,27 @@
+export default {
+    name: 'ifCond', 
+    helperFn:  function (v1, operator, v2, options) {
+        switch (operator) {
+        case '==':
+        case '===':
+            return (v1 === v2) ? options.fn(this) : options.inverse(this);
+        case '!=':
+        case '!==':
+            return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+        case '<':
+            return (v1 < v2) ? options.fn(this) : options.inverse(this);
+        case '<=':
+            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+        case '>':
+            return (v1 > v2) ? options.fn(this) : options.inverse(this);
+        case '>=':
+            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+        case '&&':
+            return (v1 && v2) ? options.fn(this) : options.inverse(this);
+        case '||':
+            return (v1 || v2) ? options.fn(this) : options.inverse(this);
+        default:
+            return options.inverse(this);
+        }
+    }
+};
---
+++ src/cli/helpers/index.ts
@@ -0,0 +1,4 @@
+import * as ifCond from './ifCond';
+import * as includes from './includes';
+
+export default [ ifCond, includes ];
