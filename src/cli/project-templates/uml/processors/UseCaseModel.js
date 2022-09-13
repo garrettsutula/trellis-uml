7a042b546ebc6c3c4807538192db5ad07daad42f
Garrett Sutula <garrettsutula@gmail.com>
2022-09-12 21:55:37 -0400

    Update uml template

--- src/cli/project-templates/uml/processors/UseCaseModel.js
+++ src/cli/project-templates/uml/processors/UseCaseModel.js
@@ -2,20 +2,72 @@ const nameToId = require('./lib/nameToId');
 
 function preprocessFn(useCaseModel) {
   const actorRefs = new Set();
+  const componentRefs = new Set();
   const {usecases = []} = useCaseModel;
   usecases.forEach((usecase) => {
     const {actors = []} = usecase;
     usecase.id = nameToId(usecase.name);
-    actors.forEach(({actor}) => actorRefs.add(actor.$ref));
+    actors.forEach(({actor, uses}) => {
+      actorRefs.add(actor.$ref);
+      uses.forEach((uiRef) => componentRefs.add(uiRef.$ref));
+    });
   });
   useCaseModel.id = nameToId(useCaseModel.name); // TODO: sanitizer util
-  useCaseModel.actors = Array.from(actorRefs.values()).map((ref) => {
-    return {$ref: ref};
-  });
+  useCaseModel.actors = [ ...actorRefs ].map((ref) => ({$ref: ref}))
+  useCaseModel.uis = [ ...componentRefs ].map((ref) => ({$ref: ref}));
   return useCaseModel;
 }
 
 function postprocessFn(useCaseModel) {
+  const { usecases = {}, uis = [] } = useCaseModel;
+  const useCasesArr = Object.values(usecases);
+  useCaseModel.components = [ ...uis, ...uis.reduce((components, ui) => {
+    const {system: { relationships = [] } = {}} = ui;
+    relationships.forEach((relationship) => {
+      if(relationship.source === ui) components.add(relationship.target);
+    });
+    return components;
+  }, new Set())];
+  useCaseModel.systems = [...new Set(useCaseModel.components.map((component) => component.system))];
+  useCaseModel.relationships = {
+    system: [...useCasesArr.reduce((relationships, {name: useCaseName, actors = []}) => {
+              actors.forEach(({actor, uses = []}) => {
+                uses.forEach((ui) => {
+                  const id = `${actor.id}-${ui.system.id}`;
+                  const relationship = relationships.get(id);
+                  if(relationship) {
+                    relationship.useCases.push(useCaseName);
+                  } else {
+                    relationships.set(id, { source: actor, target: ui.system, useCases: [ useCaseName ]});
+                  }
+                })
+              });
+              return relationships;
+            }, new Map()).values(),
+          ],
+    component: [...useCasesArr.reduce((relationships, {name: useCaseName, actors = []}) => {
+      actors.forEach(({actor, uses = []}) => {
+        uses.forEach((ui) => {
+          const id = `${actor.id}-${ui.id}`;
+          const relationship = relationships.get(id);
+          if(relationship) {
+            relationship.useCases.push(useCaseName);
+          } else {
+            relationships.set(id, { source: actor, target: ui, useCases: [ useCaseName ]});
+          }
+        })
+      });
+      return relationships;
+    }, new Map()).values(),
+    ...uis.reduce((relationships, ui) => {
+      const {system: { relationships: systemRelationships = [] } = {}} = ui;
+      systemRelationships.forEach((relationship) => {
+        if(relationship.source === ui) relationships.add(relationship);
+      });
+      return relationships;
+    }, new Set()).values()
+  ],
+  }
   return useCaseModel;
 }
 
